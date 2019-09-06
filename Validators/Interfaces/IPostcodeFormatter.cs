a9a0439499e49fa11d7c84a50bdfd5e8ca9259b9
anton young <ayoung@virtual-affairs.com>
2019-09-06 16:00:40 +0200

    Add: Class and Interface as format provider

---
+++ Validators/Interfaces/IPostcodeFormatter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Validators.Interfaces
+{
+    public interface IPostcodeFormatter
+    {
+        string Default;
+
+        string NoWhiteSpaces
+    }
+}
