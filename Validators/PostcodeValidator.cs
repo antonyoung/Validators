4ec6878587387e6ffbf421efad25fc6efd991aa4
anton young <ayoung@virtual-affairs.com>
2019-09-06 17:58:48 +0200

    CleanUp: Use constants instead doing it WET CleanUp: Some minor issues.

--- Validators/PostcodeValidator.cs
+++ Validators/PostcodeValidator.cs
@@ -7,6 +7,7 @@ using Validators.Interfaces;
 
 namespace Validators
 {
+
     /// <summary>
     ///     used as all business logic behind postal codes of European countries ( with a few exceptions, like Finland as example. )
     ///     validates and formats the postal code according to the selected country.
@@ -14,9 +15,6 @@ namespace Validators
     public class PostcodeValidator
         : IPostcodeValidator
     {
-
-        #region public properties
-
 
         /// <summary>
         ///     used as a postal code example of the set country.  
@@ -47,10 +45,44 @@ namespace Validators
             }
         }
 
-        #endregion
-
 
         #region private declarations
+
+
+        private CountryLogic _logic;
+
+        private string _input;
+
+        private bool _isValid;
+
+
+        private const string EXAMPLE_4_DIGITS = "1234";
+        private const string EXAMPLE_5_DIGITS = "12345";
+        private const string EXAMPLE_5_DIGITS_WHITESPACE = "123 45";
+
+        private const string FORMAT_ALPHANUMERIC = "<alphanumeric1> <alphanumeric2>";
+        private const string FORMAT_DIGITS = "<digits>"; 
+        private const string FORMAT_DIGITS_HYPHEN = "<digits1>-<digits2>";
+        private const string FORMAT_DIGITS_WHITESPACE = "<digits1> <digits2>";
+        private const string FORMAT_PREFIX = "<prefix><digits>";
+
+        private const string PREFIX_CROATIA = "HR-";
+        private const string PREFIX_FINLAND = "FI-";
+        private const string PREFIX_LATVIA = "LV-";
+        private const string PREFIX_LITHUANIA = "LT-";
+        private const string PREFIX_LUXEMBOURG = "L-";
+        private const string PREFIX_SLOVENIA = "SI-";
+        private const string PREFIX_SWEDEN = "SE-";
+
+        private const string REPLACE_PREFIX = "<REPLACE_PREFIX>";
+
+        private const string REGEX_4_DIGITS = "(?<digits>^[1-9][0-9]{3}$)";
+        private const string REGEX_4_DIGITS_PREFIX = "^(?<prefix>(?i)<REPLACE_PREFIX>)?(?<digits>[1-9][0-9]{3}$)";
+        private const string REGEX_5_DIGITS = "(?<digits>^[1-9][0-9]{4}$)";
+        private const string REGEX_5_DIGITS_PREFIX = "^(?<prefix>(?i)<REPLACE_PREFIX>)?(?<digits>[1-9][0-9]{4}$)";
+        private const string REGEX_5_DIGITS_WHITESPACE = @"(?<digits1>^[1-9][0-9]{2})(?<whitespace>\s?)(?<digits2>[0-9]{2}$)";
+
+
 
         /// <summary>
         ///     used as internal business logic, to differentiate between countries. 
@@ -74,135 +106,135 @@ namespace Validators
         {
             { Countries.Austria,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{3}$)",
-                    Example = "1234"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_4_DIGITS,
+                    Example = EXAMPLE_4_DIGITS
                 }
             },
             { Countries.Belgium,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{3}$)",
-                    Example = "1234"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_4_DIGITS,
+                    Example = EXAMPLE_4_DIGITS
                 }
             },
             { Countries.Bulgaria,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{3}$)",
-                    Example = "1234"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_4_DIGITS,
+                    Example = EXAMPLE_4_DIGITS
                 }
             },
             { Countries.Croatia,
                 new CountryLogic {
-                    DisplayFormat = "<prefix><digits>",
-                    RegexPattern = "^(?<prefix>(?i)HR-)?(?<digits>[1-9][0-9]{4}$)",
-                    Prefix = "HR-",
-                    Example = "HR-12345"
+                    DisplayFormat = FORMAT_PREFIX,
+                    RegexPattern = REGEX_5_DIGITS_PREFIX.Replace(REPLACE_PREFIX, PREFIX_CROATIA),
+                    Prefix = PREFIX_CROATIA,
+                    Example = $"{PREFIX_CROATIA}{EXAMPLE_5_DIGITS}"
                 }
             },
             { Countries.Cyprus,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{3}$)",
-                    Example = "1234"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_4_DIGITS,
+                    Example = EXAMPLE_4_DIGITS
                 }
             },
             { Countries.Czechia,
                 new CountryLogic {
-                    DisplayFormat = "<digits1> <digits2>",
-                    RegexPattern = @"(?<digits1>^[1-9][0-9]{2})(?<whitespace>\s?)(?<digits2>[0-9]{2}$)",
-                    Example = "123 45"
+                    DisplayFormat = FORMAT_DIGITS_WHITESPACE,
+                    RegexPattern = REGEX_5_DIGITS_WHITESPACE,
+                    Example = EXAMPLE_5_DIGITS_WHITESPACE
                 }
             },
             { Countries.Denmark,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{3}$)",
-                    Example = "1234"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_4_DIGITS,
+                    Example = EXAMPLE_4_DIGITS
                 }
             },
             { Countries.Estonia,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{4}$)",
-                    Example = "12345"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_5_DIGITS,
+                    Example = EXAMPLE_5_DIGITS
                 }
             },
             // Finland: The postal code must be preceded by ‘FI-’ = default (or by ‘AX-’ for the Åland Islands) 
             { Countries.Finland,
                 new CountryLogic {
-                    DisplayFormat = "<prefix><digits>",
+                    DisplayFormat = FORMAT_PREFIX,
                     RegexPattern = "^(?<prefix>(?i)(FI-)|(AX-))?(?<digits>[1-9][0-9]{4}$)",
-                    Prefix = "FI-",
-                    Example = "FI-12345"
+                    Prefix = PREFIX_FINLAND,
+                    Example = $"{PREFIX_FINLAND}{EXAMPLE_5_DIGITS}"
                 }
             },
             { Countries.France,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{4}$)",
-                    Example = "12345"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_5_DIGITS,
+                    Example = EXAMPLE_5_DIGITS
                 }
             },
             { Countries.Germany,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{4}$)",
-                    Example = "12345"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_5_DIGITS,
+                    Example = EXAMPLE_5_DIGITS
                 }
             },
             { Countries.Greece,
                 new CountryLogic {
-                    DisplayFormat = "<digits1> <digits2>",
-                    RegexPattern = @"(?<digits1>^[1-9][0-9]{2})(?<whitespace>\s?)(?<digits2>[0-9]{2}$)",
-                    Example = "123 45"
+                    DisplayFormat = FORMAT_DIGITS_WHITESPACE,
+                    RegexPattern = REGEX_5_DIGITS_WHITESPACE,
+                    Example = EXAMPLE_5_DIGITS_WHITESPACE
                 }
             },
             { Countries.Hungary,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{3}$)",
-                    Example = "1234"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_4_DIGITS,
+                    Example = EXAMPLE_4_DIGITS
                 }
             },
              { Countries.Italy,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{4}$)",
-                    Example = "12345"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_5_DIGITS,
+                    Example = EXAMPLE_5_DIGITS
                 }
             },
             // Ireland: not sure how correct this is?
             { Countries.Ireland,
                 new CountryLogic {
-                    DisplayFormat = "<alphanumeric1> <alphanumeric2>",
+                    DisplayFormat = FORMAT_ALPHANUMERIC,
                     RegexPattern = @"(?<alphanumeric1>(?:^[AC-FHKNPRTV-Y][0-9]{2}|D6W))(?<whitespace>\s?)(?<alphanumeric2>[0-9AC-FHKNPRTV-Y]{4}$)",
                     Example = "D22 YD82"
                 }
             },
             { Countries.Latvia,
                 new CountryLogic {
-                    DisplayFormat = "<prefix><digits>",
-                    RegexPattern = "^(?<prefix>(?i)LV-)?(?<digits>[1-9][0-9]{3}$)",
-                    Prefix = "LV-",
-                    Example = "LV-1234"
+                    DisplayFormat = FORMAT_PREFIX,
+                    RegexPattern = REGEX_4_DIGITS_PREFIX.Replace(REPLACE_PREFIX, PREFIX_LATVIA),
+                    Prefix = PREFIX_LATVIA,
+                    Example = $"{PREFIX_LATVIA}{EXAMPLE_4_DIGITS}"
                 }
             },
             { Countries.Lithuania,
                 new CountryLogic {
-                    DisplayFormat = "<prefix><digits>",
-                    RegexPattern = "^(?<prefix>(?i)LT-)?(?<digits>[1-9][0-9]{4}$)",
-                    Prefix = "LT-",
-                    Example = "LT-12345"
+                    DisplayFormat = FORMAT_PREFIX,
+                    RegexPattern = REGEX_5_DIGITS_PREFIX.Replace(REPLACE_PREFIX, PREFIX_LITHUANIA),
+                    Prefix = PREFIX_LITHUANIA,
+                    Example = $"{PREFIX_LITHUANIA}{EXAMPLE_5_DIGITS}"
                 }
             },
             { Countries.Luxembourg,
                 new CountryLogic {
-                    DisplayFormat = "<prefix><digits>",
-                    RegexPattern = "^(?<prefix>(?i)L-)?(?<digits>[1-9][0-9]{3}$)",
-                    Prefix = "L-",
-                    Example = "L-1234"
+                    DisplayFormat = FORMAT_PREFIX,
+                    RegexPattern = REGEX_4_DIGITS_PREFIX.Replace(REPLACE_PREFIX, PREFIX_LUXEMBOURG),
+                    Prefix = PREFIX_LUXEMBOURG,
+                    Example = $"{PREFIX_LUXEMBOURG}{EXAMPLE_4_DIGITS}"
                 }
             },
             { Countries.Malta,
@@ -221,72 +253,66 @@ namespace Validators
             },
             { Countries.Poland,
                 new CountryLogic {
-                    DisplayFormat = "<digits1>-<digits2>",
+                    DisplayFormat = FORMAT_DIGITS_HYPHEN,
                     RegexPattern = @"(?<digits1>^[1-9][0-9]{1})(?<hyphen>-?)(?<digits2>[0-9]{3}$)",
                     Example = "12-345"
                 }
             },
             { Countries.Portugal,
                 new CountryLogic {
-                    DisplayFormat = "<digits1>-<digits2>",
+                    DisplayFormat = FORMAT_DIGITS_HYPHEN,
                     RegexPattern = @"(?<digits1>^[1-9][0-9]{3})(?<hyphen>-?)(?<digits2>[0-9]{3}$)",
                     Example = "1234-567"
                 }
             },
             { Countries.Romania,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
+                    DisplayFormat = FORMAT_DIGITS,
                     RegexPattern = "(?<digits>^[1-9][0-9]{5}$)",
                     Example = "123456"
                 }
             },
             { Countries.Slovakia,
                 new CountryLogic {
-                    DisplayFormat = "<digits1> <digits2>",
-                    RegexPattern = @"(?<digits1>^[1-9][0-9]{2})(?<whitespace>\s?)(?<digits2>[0-9]{2}$)",
-                    Example = "123 45"
+                    DisplayFormat = FORMAT_DIGITS_WHITESPACE,
+                    RegexPattern = REGEX_5_DIGITS_WHITESPACE,
+                    Example = EXAMPLE_5_DIGITS_WHITESPACE
                 }
             },
             { Countries.Slovenia,
                 new CountryLogic {
-                    DisplayFormat = "<prefix><digits>",
-                    RegexPattern = "^(?<prefix>(?i)SI-)?(?<digits>[1-9][0-9]{3}$)",
-                    Prefix = "SI-",
-                    Example = "SI-1234"
+                    DisplayFormat = FORMAT_PREFIX,
+                    RegexPattern = REGEX_4_DIGITS_PREFIX.Replace(REPLACE_PREFIX, PREFIX_SLOVENIA),
+                    Prefix = PREFIX_SLOVENIA,
+                    Example = $"{PREFIX_SLOVENIA}{EXAMPLE_4_DIGITS}"
                 }
             },
             { Countries.Spain,
                 new CountryLogic {
-                    DisplayFormat = "<digits>",
-                    RegexPattern = "(?<digits>^[1-9][0-9]{4}$)",
-                    Example = "12345"
+                    DisplayFormat = FORMAT_DIGITS,
+                    RegexPattern = REGEX_5_DIGITS,
+                    Example = EXAMPLE_5_DIGITS
                 }
             },
             { Countries.Sweden,
                 new CountryLogic {
                     DisplayFormat = "<prefix><digits1> <digits2>",
                     RegexPattern = @"^(?<prefix>(?i)SE-)?(?<digits1>[1-9][0-9]{2})(?<whitespace>\s?)(?<digits2>[0-9]{2}$)",
-                    Prefix = "SE-",
-                    Example = "SE-123 45"
+                    Prefix = PREFIX_SWEDEN,
+                    Example = $"{PREFIX_SWEDEN}{EXAMPLE_5_DIGITS_WHITESPACE}"
                 }
             },
             // United Kingdom: not sure how correct this is?
             { Countries.UnitedKingdom,
                 new CountryLogic {
-                    DisplayFormat = "<alphanumeric1> <alphanumeric2>",
+                    DisplayFormat = FORMAT_ALPHANUMERIC,
                     RegexPattern = @"^(?<alphanumeric1>(([A-Z][0-9]{1,2})|(([A-Z][A-HJ-Y][0-9]{1,2})|(([A-Z][0-9][A-Z])|([A-Z][A-HJ-Y][0-9]?[A-Z])))))(?<whitespace>\s?)(?<alphanumeric2>[0-9][A-Z]{2}$)",
                     Example = "DN55 1PT"
                 }
             }
          };
 
-        private CountryLogic _logic;
-
-        private string _input;
-        private bool _isValid;
-
         #endregion
-
 
 
         /// <summary>
@@ -296,7 +322,7 @@ namespace Validators
         ///     used as the postcode to be validated with the default country The Netherlands and formatted as default. 
         /// </param>
         /// <param name="result">
-        ///     used as the result postcode 
+        ///     used as the result of the formatted postcode with default country The Netherlands and with default formatter.
         /// </param>
         /// <exception cref="ArgumentException"/>
         /// <exception cref="ArgumentNullException"/>
@@ -346,13 +372,15 @@ namespace Validators
         ///     used as the result postcode 
         /// </param>
         /// <exception cref="ArgumentException"/>
-        /// <exception cref="ArgumentNullException"/>
         /// <exception cref="RegexMatchTimeoutException"/>
         /// <returns>
         ///     <seealso cref="bool"/> is valid or not and as out the formatted postcode.
         /// </returns>
         public bool TryParse(string value, Countries country, RemoveFormatter formatter, out string result)
-        { 
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException(nameof(value));
+
             _input = result = value.Trim();
             
             if (!_countries.TryGetValue(country, out _logic))
@@ -362,11 +390,9 @@ namespace Validators
 
             IsValid = match.Success;
 
-            if (!_isValid) return false;
+            if (_isValid) result = Format(match, formatter);
 
-            result = Format(match, formatter);
-
-            return true;
+            return _isValid;
         }
 
 
@@ -380,7 +406,7 @@ namespace Validators
         ///     used as the formatter how to format the result.
         /// </param>
         /// <returns>
-        ///     postcode with provide formatter. 
+        ///     postcode as with provided formatter. 
         /// </returns>
         private string Format(Match match, RemoveFormatter formatter)
         {
