4d5541962b59801657db85b6f07d120e725ccb7e
anton young <ayoung@virtual-affairs.com>
2019-09-15 13:07:03 +0200

    Refactor: Use of new interface, add comments.

--- Validators/IbanValidator.cs
+++ Validators/IbanValidator.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -14,38 +12,58 @@ namespace Validators
 {
 
     /// <summary>
-    /// 
+    ///     used as all business logic behind iban of European countries.
+    ///     validates and formats the iban according to the iban country.
     /// </summary>
     public class IbanValidator
         : IIbanValidator
     {
+
+        /// <summary>
+        ///     used as internal business rules of European iban.
+        /// </summary>
         private readonly IIbanModel _model;
+
+
+        /// <summary>
+        ///      used as internal business rules of iban of selected country.
+        /// </summary>
         private IbanRuleSetModel _logic;
 
-        private const string _alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-       
+
+        /// <summary>
+        ///     used as internal business logic for sanity check, based on index.
+        /// </summary>
+        private const string _alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+
+        /// <summary>
+        ///         used as constructor to initiliaze the class with the internal business rules of all internal iban.
+        /// </summary>
         public IbanValidator() => _model = new IbanModel();
 
 
         /// <summary>
-        /// 
+        ///     used as an iban example of a country.
+        /// </summary>
+        public string Example { get => _logic.Example; }
+
+
+        /// <summary>
+        ///     used as error message in case of <seealso cref="IsValid"/> = false;
         /// </summary>
         public string ErrorMessage { get; private set; }
 
 
         /// <summary>
-        /// 
+        ///     used as the country of the iban value. 
         /// </summary>
         public Countries Country => _logic.Country;
 
 
-        public string SwiftCode => throw new NotImplementedException();
-
-        public int AccountNumber => throw new NotImplementedException();
-
-
         /// <summary>
-        /// 
+        ///     used to validate given iban.
+        ///     when false an <seealso cref="ErrorMessage"/> will be given.
         /// </summary>
         public bool IsValid
         {
@@ -53,43 +71,88 @@ namespace Validators
         }
 
 
-        private int CharAsInt(char value)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">
+        /// 
+        /// </param>
+        /// <param name="result">
+        /// 
+        /// </param>
+        /// <returns>
+        /// 
+        /// </returns>
+        public bool Validate(string value, out string result)
         {
-            var index = _alphabet.IndexOf(value);
+            result = value.Trim()
+                ?? throw new ArgumentException(nameof(value));
 
-            // char not found!
-            if (index == -1)
-                throw new ArgumentOutOfRangeException(nameof(value));
+            // use TwoLetterISORegionName as key
+            if (!_model.Rules.TryGetValue(value.Substring(0, 2).ToUpperInvariant(), out _logic))
+                throw new ArgumentException("no matching country found.");
 
-            // where A = 10, B = 11, ..., Z = 35
-            return index += 10;
+            var match = Regex.Match(result, _logic.RegexPattern);
+
+            IsValid = match.Success;
+
+            if (IsValid)
+            {
+                IsValid = IsSanityValid(match);
+
+                // todo: format match value;
+            }
+
+            return IsValid;
         }
-         
 
-        private int CharAsInt(char[] values)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="match">
+        /// 
+        /// </param>
+        /// <returns>
+        /// 
+        /// </returns>
+        private bool IsSanityValid(Match match)
         {
-            var stringBuilder = new StringBuilder(values.Length * 2);
+            double checkSum = 0;
+            GenericIndexer<byte> sanityIndexer = CreateSanityIndexer(match);
 
-            foreach (var number in values)
-                stringBuilder.Append(CharAsInt(number));
+            // RESEARCH: System.ReadOnlySpan<char> => net core 3.0 new implementation?
+            foreach (byte value in sanityIndexer)
+            {
+                checkSum *= 10;
+                checkSum += value;
+                checkSum %= 97;
+            }
 
-            if (!int.TryParse(stringBuilder.ToString(), out int result))
-                throw new ArgumentException(nameof(values));
-
-            return result;
+            return checkSum == 1;
         }
 
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="match">
+        /// 
+        /// </param>
+        /// <returns>
+        /// 
+        /// </returns>
         private GenericIndexer<byte> CreateSanityIndexer(Match match)
         {
             var country = CharAsInt(match.Groups.Single(group
                 => group.Name == "country").Value.ToCharArray());
 
-            //if (match.Groups.Contains(item => item.Name == "name")
-            //{
-                var name = CharAsInt(match.Groups.SingleOrDefault(group
+            int name = 0;
+            if (match.Groups.Any(item => item.Name.Equals("name", StringComparison.OrdinalIgnoreCase)))
+            {
+                name = CharAsInt(match.Groups.SingleOrDefault(group
                     => group.Name == "name").Value.ToCharArray());
-            //}
+            }
 
             var formatAsNumbers = _logic.SanityFormat;
 
@@ -100,7 +163,6 @@ namespace Validators
 
                 if (group.Name.Equals("name", StringComparison.OrdinalIgnoreCase))
                     formatAsNumbers = formatAsNumbers.Replace(string.Format("<{0}>", group.Name), name.ToString());
-
 
                 if (!string.IsNullOrWhiteSpace(group.Value))
                     formatAsNumbers = formatAsNumbers.Replace(string.Format("<{0}>", group.Name), group.Value);
@@ -115,54 +177,52 @@ namespace Validators
                 );
         }
 
-        private bool IsSanityValid(Match match)
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">
+        /// 
+        /// </param>
+        /// <returns>
+        /// 
+        /// </returns>
+        private int CharAsInt(char[] value)
         {
-            double checkSum = 0;
-            GenericIndexer<byte> sanityIndexer = CreateSanityIndexer(match);
+            var stringBuilder = new StringBuilder(value.Length * 2);
 
-            // RECEARCH: System.ReadOnlySpan<char> => net core 3.0 new implementation?
-            foreach (byte value in sanityIndexer)
-            {
-                checkSum *= 10;
-                checkSum += value;
-                checkSum %= 97;
-            }
+            foreach (var alphaNumber in value)
+                stringBuilder.Append(CharAsInt(alphaNumber));
 
-            return checkSum == 1;
+            if (!int.TryParse(stringBuilder.ToString(), out int result))
+                throw new ArgumentException(nameof(value));
+
+            return result;
         }
 
 
-        public bool Validate(string value, out string result)
+        /// <summary>
+        ///     used as to convert a letter to an int.
+        /// </summary>
+        /// <param name="value">
+        ///     used as the char, that has to be converted.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     in case <paramref name="value"/> has not been found.
+        /// </exception>
+        /// <returns>
+        ///     int value of <paramref name="value"/> to be used for the sanity check.
+        /// </returns>
+        private int CharAsInt(char value)
         {
-            result = value.Trim()
-                ?? throw new ArgumentException(nameof(value));
+            var index = _alphabet.IndexOf(value);
 
-            // use TwoLetterISORegionName as key
-            if (!_model.Rules.TryGetValue(value.Substring(0, 2).ToUpperInvariant(), out _logic))
-                throw new ArgumentException("no mathichint country found.");
+            // => value not found!
+            if (index == -1)
+                throw new ArgumentOutOfRangeException(nameof(value));
 
-            var match = Regex.Match(result, _logic.RegexPattern);
-
-            IsValid = match.Success;
-
-            if(IsValid)
-            {
-                IsValid = IsSanityValid(match);
-
-                // todo: format match value;
-            }          
-
-            return IsValid;
+            // => where A = 10, B = 11, ..., Z = 35
+            return index;
         }
-
-        //int IIbanValidator.CharAsInt(char value)
-        //{
-        //    throw new NotImplementedException();
-        //}
-
-        //int IIbanValidator.CharAsInt(char[] value)
-        //{
-        //    throw new NotImplementedException();
-        //}
     }
 }
