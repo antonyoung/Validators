f6d6c058099f4f8f4bc1d624be0a3c446497d4ec
anton young <ayoung@virtual-affairs.com>
2019-09-16 19:58:35 +0200

    Add: Comments

--- Validators/Models/IbanRuleSetModel.cs
+++ Validators/Models/IbanRuleSetModel.cs
@@ -3,19 +3,47 @@
 
 namespace Validators.Models
 {
+
+    /// <summary>
+    ///     interface used to define the logic needed for iban validation.
+    /// </summary>
     public struct IbanRuleSetModel
         : IRuleSet
     {
-        public string RegexPattern { get ; set; }
 
+        /// <summary>
+        ///     used as the country of the iban value. 
+        /// </summary>
+        public Countries Country { get; set; }
+
+
+        /// <summary>
+        ///     used as how to format the given iban value, based on <see cref="System.Text.RegularExpressions.Group.Name"/>.
+        /// </summary>
         public string DisplayFormat { get; set; }
 
+
+        /// <summary>
+        ///     used as an example as iban value of <see cref="Country"/>.
+        /// </summary>
         public string Example { get; set; }
 
-        public string SanityFormat { get; set; }
 
+        /// <summary>
+        ///     used as additional internal validation of expected length of iban value.
+        /// </summary>
         public int Length { get; set; }
 
-        public Countries Country { get; set; }
+
+        /// <summary>
+        ///     used as internal validation of the sanity check, based on <see cref="Country"/> and <see cref="System.Text.RegularExpressions.Group.Name"/>.
+        /// </summary>
+        public string SanityFormat { get; set; }
+
+
+        /// <summary>
+        ///     used as <see cref="System.Text.RegularExpressions.Group.Name"/> to validate, sanity check and format the iban value.
+        /// </summary>
+        public string RegexPattern { get; set; }
     }
 }
