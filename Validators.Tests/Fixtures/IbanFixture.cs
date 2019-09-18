92de4169c5b6ddbec5acfb00387c52d8c894033e
anton young <ayoung@virtual-affairs.com>
2019-09-18 20:20:27 +0200

    Update: Add xUnit fixtures

---
+++ Validators.Tests/Fixtures/IbanFixture.cs
@@ -0,0 +1,24 @@
+﻿namespace Validators.Tests.Fixtures
+{
+
+    using Validators.Interfaces;
+
+
+    /// <summary>
+    ///     used as xUnit fixture for <see cref="IbanValidator"/>
+    /// </summary>
+    public class IbanFixture
+    {
+
+        /// <summary>
+        ///     used as the interface <see cref="IIbanValidator"/> to be used for the test classes.
+        /// </summary>
+        public IIbanValidator Validator;
+
+
+        /// <summary>
+        ///     used as constructor logic to define the <see cref="Validator"/>
+        /// </summary>
+        public IbanFixture() => Validator = new IbanValidator();
+    }
+}
