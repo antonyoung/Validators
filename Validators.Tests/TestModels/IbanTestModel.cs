3064edf9653a57767661d0f01763e99af0a9edfd
anton young <ayoung@virtual-affairs.com>
2019-09-20 19:22:38 +0200

    Add: TestModels

---
+++ Validators.Tests/TestModels/IbanTestModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Validators.Interfaces;
+
+
+namespace Validators.Tests.TestModels
+{
+    public class IbanTestModel
+        : IIbanValidator
+    {
+        public string Value { get; set; }
+
+        public string AccountNumber { get; set; }
+
+        public byte CheckDidgets { get; set; }
+
+        public Countries Country { get; set; }
+
+        public string Example { get; set; }
+
+        public string ErrorMessage { get; set; }
+
+        public bool IsValid { get; set; }
+
+        public string NationalBankCode { get; set; }
+
+        public string NationalBranchCode { get; set; }
+
+        public byte? NationalCheckDidget { get; set; }
+
+        public bool Validate(string value, out string result) => throw new NotImplementedException();
+    }
+}
